3158e4631b0994c8c4eb40d8fe37f58c9d2c6986
root <root@vm-1.ru-central1.internal>
2023-03-11 15:37:02 +0000

    primary commit

---
+++ test.js
@@ -0,0 +1,12 @@
+class Number {
+constructor(name, code) {
+this.name = name;
+this.name = code;
+}
+}
+
+const allNumbers = [
+new Number('one', '1'),
+new Number('two', '2'),
+new Number('three', '3'),
+];
